d8762e4b298e11d4ae02a2a7cd43cb29a17c56c4
vaishu <automationtest1010@gmail.com>
2025-12-22 22:38:06 +0530

    Comments done

---
+++ src/ex_01_Java_Basics/Lab002_Comments.java
@@ -0,0 +1,23 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Comments
+{
+    public static void main(String[] args)
+    {
+        // This is a comment
+        // This is single line comment
+        // this code will not executed.
+        // This is also a comment
+
+
+        // This is multi-line comment
+         /**
+         * Author : Pramod Dutta
+         * Course :  ATB15x
+         * Learning :  Java
+         **/
+
+
+    }
+
+}
