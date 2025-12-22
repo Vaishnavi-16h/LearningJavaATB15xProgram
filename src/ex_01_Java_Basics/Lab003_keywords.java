8ed5aab4fea04545d7dd286f13c6f4a4390b7047
vaishu <automationtest1010@gmail.com>
2025-12-22 22:56:11 +0530

    keywords done

---
+++ src/ex_01_Java_Basics/Lab003_keywords.java
@@ -0,0 +1,20 @@
+package ex_01_Java_Basics;
+
+public class Lab003_keywords
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Keywords!");    //In this prog following are keywords
+        // package, public, class, static, void
+
+
+        int a = 10;
+        //int-> Keyword
+        //but
+        //Int -> not keyword
+
+
+
+
+    }
+}
