bc80948d6303df18722bcc9e668f3f87189eaacb
vaishu <automationtest1010@gmail.com>
2025-12-22 23:21:49 +0530

    main-method done

---
+++ src/ex_01_Java_Basics/Lab007_Main_Method.java
@@ -0,0 +1,35 @@
+package ex_01_Java_Basics;
+
+public class Lab007_Main_Method
+{
+    public static void main(String[] args)
+    {
+
+        /**
+         *
+         public static void main(String[] args)
+         {
+         }
+
+        public static void main(String[] args)
+         {
+         }
+
+        public static void main(String[] args)
+         {
+         }
+
+
+        public static void main(String args)........... duplicate or dummy or doppel-ganger
+         {
+         }
+
+
+        public static void Main(String[] args)............ original
+         {
+         }
+
+    **/
+    }
+
+}
