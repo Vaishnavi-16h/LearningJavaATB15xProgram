1ee634d1666c6f21a9cf2d601aa48eef1db38b47
vaishu <automationtest1010@gmail.com>
2025-12-24 23:15:56 +0530

    variables done

---
+++ src/ex_01_Java_Basics/Lab008_Variables.java
@@ -0,0 +1,95 @@
+package ex_01_Java_Basics;
+
+public class Lab008_Variables
+{
+    public static void main(String[] args) {
+
+        // data_type variable_name =  variable_value
+        // Variable - Local Variable
+
+        //Ex 1.........................................................
+        byte age_pramod = 45;
+        // byte -> data type - type of container
+        //  age_sunil ->  variable_name |  identifier
+        // = -> Assignment Operator ?
+        // 45 -> Variable Value |  Literal
+        age_pramod = 112;
+        age_pramod = 119;
+        System.out.println(age_pramod);
+
+
+        //Ex 2...........................................................
+        int a = 10;
+        int a1 = -10;
+        a = 65+1;
+        System.out.println(a);
+
+
+        //Ex 3..........................................................
+        // I want to the age of a person
+        // byte, short, int?
+        // int age = 122;
+        //short age_s = 122;
+        byte age_b = 122; // 127
+         //byte age_c = 128;
+        short s = 128;
+
+
+        //Ex 4..........................................................
+        //int _ = 65;
+        int $ = 65;
+        int year = 2025;
+        int this_is_a_vari_long_name_in_the_class_hello_$_akakak = 89;
+
+
+        //Ex 5..........................................................
+        int _123 = 123;
+        System.out.println(_123);
+
+
+        //Ex 6..........................................................
+        //int enum = 10;
+        int Enum = 10;
+        System.out.println(Enum);
+        int pramod$dutta = 90;
+        int Lab020_Interview_QnA2 = 123;
+
+
+        //Ex 7..........................................................
+        boolean is_pramod_married = true;
+        boolean pramod_has_audi = false;
+        boolean this_is_a_long_name_that_student_asked_from_us_that_is_it_possible = true;
+
+
+        //Ex 8....All....................................................
+        byte b = 10;
+        short ss = 10;
+        int i = 10;
+        char c = 'A';
+        char c1 = '@';
+        char c2 = '_';
+
+        //int phone = 9876543210;
+        long phone = 9876543210l;
+        long phone_1 = 9876543210L;
+
+        float f = 3.14f;
+        float f1 = 3.14F;
+
+        double d = 3.12345678987;
+
+
+        //Ex 9..........................................................
+        long age = 10L;
+        char ch = 10;
+        System.out.println(c);
+        System.out.println(age);
+
+        String st = "Pramod"; // Non Primitive.
+        // Pramod -> p,r,a,m,o,d - chars-  String is bunch of chars.
+
+
+
+    }
+
+}
---
+++ src/task/Nested_Ternary_eg1.java
@@ -0,0 +1,17 @@
+package task;
+
+public class Nested_Ternary_eg1
+{
+    public static void main(String[] args)
+    {
+        int n1 = 2;
+        int n2 = 9;
+        int n3 = -11;
+        System.out.println("MAX OUT OF THREE");
+
+        //int result = (n1 > n2) ? n1 : (n2 > n3) ? n2 : n3; //wrong 5 3 10...use this and try it will wrong
+        int result = (n1 > n2) ? (n1 > n3 ? n1 : n3) : (n2 > n3 ? n2 : n3);
+        System.out.println("Result="+result);
+
+    }
+}
