9b5c300be672eec474adea99af22748c54d24c31
vaishu <automationtest1010@gmail.com>
2025-12-26 22:22:59 +0530

    Grade displaying for score done

---
+++ src/task/Grade_Display.java
@@ -0,0 +1,41 @@
+package task;
+
+import java.util.Scanner;
+
+public class Grade_Display
+{
+    public static void main(String[] args)
+    {
+//        A: 90-100
+//        B: 80-89
+//        C: 70-79
+//        D: 60-69
+//        F: 0-59
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter your score=");
+        int score = s.nextInt();
+
+        if(score>=90 && score<=100)
+        {
+            System.out.println("A Grade");
+        }
+        else if(score>=80 && score<=89)
+        {
+            System.out.println("B Grade");
+        }
+        else if(score>=70 && score<=79)
+        {
+            System.out.println("B Grade");
+        }
+        else if(score>=60 && score<=69)
+        {
+            System.out.println("B Grade");
+        }
+        else
+        {
+            System.out.println("Fail");
+        }
+
+    }
+}
