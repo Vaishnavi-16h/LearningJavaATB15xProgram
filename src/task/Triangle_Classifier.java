1cdda2702e5f3f2cddbf23fd47f7a93cf141c010
vaishu <automationtest1010@gmail.com>
2025-12-26 16:29:27 +0530

    triangle classifier done

---
+++ src/task/Triangle_Classifier.java
@@ -0,0 +1,31 @@
+package task;
+
+import java.util.Scanner;
+
+public class Triangle_Classifier
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter side1=");
+        int side1 = s.nextInt();
+        System.out.print("Enter side2=");
+        int side2 = s.nextInt();
+        System.out.print("Enter side3=");
+        int side3 = s.nextInt();
+
+        if(side1==side2 && side1==side3)
+        {
+            System.out.println("Equilateral Triangle.");
+        }
+        else if(side1==side2 || side1==side3 || side2==side3)
+        {
+            System.out.println("Isosceles Triangle.");
+        }
+        else
+        {
+            System.out.println("Scalene Triangle.");
+        }
+
+    }
+}
